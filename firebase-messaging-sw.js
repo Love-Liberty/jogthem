b29696332715864c743455613995e1ad63315a74
Love-Liberty <48633589+Love-Liberty@users.noreply.github.com>
2019-03-25 00:14:16 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,51 @@
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+importScripts('/__/firebase/init.js'); //Error:app already exists, but if removed Error:method only available in window
 
-      // Initialize Firebase
-      var config = {
-        apiKey: "AIzaSyDe4TVROJcbVJfSZ5WkTZdfK7bWXlnx18Q",
-        authDomain: "jogthem.firebaseapp.com",
-        databaseURL: "https://jogthem.firebaseio.com",
-        projectId: "jogthem",
-        storageBucket: "jogthem.appspot.com",
-        messagingSenderId: "593287500713"
-      };
-      firebase.initializeApp(config);
+// Initialise Firebase (hopefully with the correct details)
 
-/*2016 version
-//Initialize the Firebase app in the service worker by passing in the messagingSenderId.
-firebase.initializeApp({
-    'messagingSenderId': 'YOUR-SENDER-ID'
-  })
+  var config = {
+    apiKey: "AIzaSyDe4TVROJcbVJfSZ5WkTZdfK7bWXlnx18Q", //checked
+    authDomain: "jogthem.firebaseapp.com",
+    databaseURL: "https://jogthem.firebaseio.com",
+    projectId: "jogthem",
+    storageBucket: "jogthem.appspot.com",
+    messagingSenderId: "593287500713"       //checked
+  };
+firebase.initializeApp(config);
 
+/*
+ firebase.initializeApp({
+  'messagingSenderId': '593287500713'  //does this work? No difference.
+})
 */
+  console.log('Messaging-sw.js called ');//fails
+
+/*
+Retrieve an instance of Firebase Messaging so that it can handle background messages.
+*/
+const messaging = firebase.messaging()
+
+messaging.onMessage(function(payload) {
+  console.log('Message received. ', payload);
+  // ...
+});
+
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  /*const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+    
+  };
+*/
+ // return self.registration.showNotification(notificationTitle,
+ //     notificationOptions);
+});
+
 
 
 /*  the below seems to log errors "messaging is only available in a window context.  Failed to update a serviceWorker"
@@ -44,67 +71,5 @@ messaging.onMessage(function(payload){
 
 */
 
-/*2016 version
 
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  return self.registration.showNotification(notificationTitle,
-      notificationOptions);
-});
-
-
-
-*/
-
-
-      
-/* messaging.onMessage(function(payload) 
-console.log('onMessage: ', payload);
-
-
-{
-  "message":{ 
-    "token":"e74BYS0qb7Y:APA91bFfB-67cS_D8cfeSt6P9UjjO0ogHYeHmUbhgIpYDyG-eK2raE9w1RejjUJT_9ZGw-y0NdvyGVZ6riskWwr883X1uSkjlu3fKseeie0xb4Pp1mb-NM9Y7oNTZvtZZQyC_H1fhmdD",
-    "notification":{
-      "title":"Portugal vs. Denmark",
-      "body":"great match!"
-    }
-  }
-}
-
-
-{
-"message": {
-    "token": "YOUR_TARGET_APP_TOKEN",
-    "data": {
-        "title": "FCM Message",
-        "body": "This is a Jog",
-        "icon": "https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_28dp.png",
-        "link": "https://jogthem.firebaseapp.com"
-    }
-}
-the following is rejected in CLI:
-curl -X POST -H "Authorization: key=AIzaSyDe4TVROJcbVJfSZ5WkTZdfK7bWXlnx18Q" -H "Content-Type: application/json" -d '{
-  "notification": {
-    "title": "Portugal vs. Denmark",
-    "body": "5 to 1",
-    "icon": "firebase-logo.png",
-    "click_action": "http://localhost:8081"
-  },
-  "to": "e74BYS0qb7Y:APA91bFfB-67cS_D8cfeSt6P9UjjO0ogHYeHmUbhgIpYDyG-eK2raE9w1RejjUJT_9ZGw-y0NdvyGVZ6riskWwr883X1uSkjlu3fKseeie0xb4Pp1mb-NM9Y7oNTZvtZZQyC_H1fhmdD"
-}' "https://fcm.googleapis.com/fcm/send"
-
-
-<!-- 
-e74BYS0qb7Y:APA91bFfB-67cS_D8cfeSt6P9UjjO0ogHYeHmUbhgIpYDyG-eK2raE9w1RejjUJT_9ZGw-y0NdvyGVZ6riskWwr883X1uSkjlu3fKseeie0xb4Pp1mb-NM9Y7oNTZvtZZQyC_H1fhmdD
--->
-
-*/
 
