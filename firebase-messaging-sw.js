7f24de47411d99a39d7ca2fdfb4d1c3efedd702f
Love-Liberty <48633589+Love-Liberty@users.noreply.github.com>
2019-04-15 21:14:50 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 
 firebase.initializeApp({
-  'messagingSenderId': '593287500713' 
+  'messagingSenderId': '123456789012'  //replace with the senderId for your project 
 });
 
 const messaging = firebase.messaging();
