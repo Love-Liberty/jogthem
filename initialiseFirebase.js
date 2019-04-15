5ec9490fad26670650ab3f73d11b8cb62d0837e2
Love-Liberty <48633589+Love-Liberty@users.noreply.github.com>
2019-04-15 20:58:44 +0200

    Create initialiseFirebase.js

---
+++ initialiseFirebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase   //replace with data from your firebase project
+    var config = {
+      apiKey: "ZZZZZZDe4TVROYYYYYYSZ5WkTZdfK7bWXXXXXXX", 
+      authDomain: "xxxxxx.firebaseapp.com",
+      databaseURL: "https://xxxxx.firebaseio.com",
+      projectId: "xxxxx",
+      storageBucket: "xxxxx.appspot.com",
+      messagingSenderId: "123456789012"
+    };
+    firebase.initializeApp(config);
